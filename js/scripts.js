ef420419e3fd8f7717996b03b20c9b666663510e
Ebru Rice <ebruerenb@gmail.com>
2021-11-10 10:43:19 -0800

    Fix JS to uppercase the string

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-    const shopping = $("#items").val().split(" ");
+    const shopping = $("#items").val().toUpperCase().split(" ");
+    if (shopping === 0) {
+      shopping.trim();
+    }
     const array = shopping.sort();
     array.forEach(function(item) {
       $("#list").append("<li>" + item + "</li>");
