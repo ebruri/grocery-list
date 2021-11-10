7b43b6638123b2e2dc1bed0b1ebc572fa138a517
Ebru Rice <ebruerenb@gmail.com>
2021-11-10 11:20:06 -0800

    hide the form after submit

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,10 @@ $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
     const shopping = $("#items").val().toUpperCase().split(" ");
-    if (shopping === 0) {
-      shopping.trim();
-    }
     const array = shopping.sort();
     array.forEach(function(item) {
       $("#list").append("<li>" + item + "</li>");
+      $("#form").hide();
     });
   });
 });
